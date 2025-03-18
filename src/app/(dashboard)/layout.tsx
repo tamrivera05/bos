36aa77cc285acc4781372edcac243a12c8eba0e4
xdjeremy <jeremy.fran@icloud.com>
2025-03-19 03:30:40 +0800

    Feat: Update DashboardLayout to use server components and implement authentication checks

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,17 +1,32 @@
-"use client";
+'use server';
 
-import type React from "react";
-import { MainNav } from "../../components/MainNav/mainNav";
+import { ApiResponse } from '@/lib/apiFetch';
+import { checkAuthenticationStatus } from '@/lib/auth';
+import { serverApiFetch } from '@/lib/serverApiFetch';
+import { notFound, redirect } from 'next/navigation';
+import { ReactNode } from 'react';
+import { User } from '../../../types/database';
+import { MainNav } from '../../components/MainNav/mainNav';
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const DashboardLayout = async ({ children }: { children: ReactNode }) => {
+  const isAuthenticated = await checkAuthenticationStatus();
+
+  if (!isAuthenticated) {
+    redirect('/log-in');
+  }
+
+  // check if admin
+  const { data } = await serverApiFetch<ApiResponse<User>>('/user');
+  const isAdmin = data?.data?.user.role === 'admin';
+
+  if (!isAdmin) notFound();
+
   return (
     <div className="relative flex min-h-screen flex-col">
       <MainNav />
       <main className="flex-1">{children}</main>
     </div>
   );
-}
+};
+
+export default DashboardLayout;
